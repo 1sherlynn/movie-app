98e2ea9a16c01b2c5641a33f5e169fb0bfe6f449
sherlynn <1sherlynn@gmail.com>
2019-03-07 23:50:17 +0800

    add  movie components

--- src/modules/movie-browser/movie-browser.actions.js
+++ src/modules/movie-browser/movie-browser.actions.js
@@ -11,7 +11,7 @@ export const getTopMovies = (page) => createAsyncActionCreator(
   // actionType
   keys.GET_TOP_MOVIES,
   // requestFn
-  movieService.getTopMoviesRequest, 
+  movieService.getTopMovies, 
   // requestParams
   {page}
 );
